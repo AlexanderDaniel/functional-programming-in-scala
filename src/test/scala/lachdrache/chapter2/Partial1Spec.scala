c3bd50da04ea6d2236d1acf9a45851403bc11da6
Alexander Daniel <alexander.daniel@gmx.at>
2014-02-13 18:21:27 +0100

    chapter 2: partial1

--- src/test/scala/lachdrache/chapter2/Partial1Spec.scala
+++ src/test/scala/lachdrache/chapter2/Partial1Spec.scala
@@ -4,18 +4,23 @@ import org.specs2.mutable.Specification
 
 class Partial1Spec extends Specification {
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
-
   object UsingUnderscore {
-    def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+    def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
       f(a, _)
   }
 
+  object UsingTypeInference {
+    def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
+      b => f(a, b)
+  }
+
+
   "partial1" should {
 
-    def f2(a: Int, b: Int) = a+b
+    def f2(a: Int, b: Int) = a + b
 
     "return a function with one parameter" in {
       val f1: (Int) => Int = partial1(3, f2)
@@ -23,9 +28,10 @@ class Partial1Spec extends Specification {
     }
   }
 
+
   "partial1 using underscore notation" should {
 
-    def f2(a: Int, b: Int) = a+b
+    def f2(a: Int, b: Int) = a + b
 
     "return a function with one parameter" in {
       val f1: (Int) => Int = UsingUnderscore.partial1(3, f2)
