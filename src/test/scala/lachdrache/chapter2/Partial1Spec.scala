5f77dea81027a2d3c2843730599b052b539926c7
Alexander Daniel <alexander.daniel@gmx.at>
2014-02-13 18:13:49 +0100

    chapter 2: partial1

---
+++ src/test/scala/lachdrache/chapter2/Partial1Spec.scala
@@ -0,0 +1,36 @@
+package lachdrache.chapter2
+
+import org.specs2.mutable.Specification
+
+class Partial1Spec extends Specification {
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+    (b: B) => f(a, b)
+
+
+  object UsingUnderscore {
+    def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+      f(a, _)
+  }
+
+  "partial1" should {
+
+    def f2(a: Int, b: Int) = a+b
+
+    "return a function with one parameter" in {
+      val f1: (Int) => Int = partial1(3, f2)
+      f1(10) === 13
+    }
+  }
+
+  "partial1 using underscore notation" should {
+
+    def f2(a: Int, b: Int) = a+b
+
+    "return a function with one parameter" in {
+      val f1: (Int) => Int = UsingUnderscore.partial1(3, f2)
+      f1(10) === 13
+    }
+  }
+
+}
