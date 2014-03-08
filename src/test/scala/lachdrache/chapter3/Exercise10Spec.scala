814062520b408ccf69756233cec45a83fce98c0b
Alexander Daniel <alexander.daniel@gmx.at>
2014-03-08 20:56:33 +0100

    chapter 3: exercise 10 part 2

--- src/test/scala/lachdrache/chapter3/Exercise10Spec.scala
+++ src/test/scala/lachdrache/chapter3/Exercise10Spec.scala
@@ -3,6 +3,9 @@ package lachdrache.chapter3
 import org.specs2.mutable.Specification
 import lachdrache.chapter3.List._
 
+/**
+ * [[https://github.com/pchiusano/fpinscala/blob/master/answerkey/datastructures/10.answer.scala answer]]
+ */
 class Exercise10Spec extends Specification {
 
   "oneTo (constructing a List from 1 to N)" should {
@@ -18,6 +21,13 @@ class Exercise10Spec extends Specification {
   "length which uses foldRight" should {
     "create stack overflow for a big list" in {
       List.length(oneTo(10000)) should throwA[StackOverflowError]
+    }
+  }
+
+  "foldLeft" should {
+    "be tail recursive (i.e. no stack overflow)" in {
+      def lengthLeft[A](l: List[A]) = foldLeft(l,0)((z,_) => z+1)
+      lengthLeft(oneTo(10000)) === 10000
     }
   }
 
