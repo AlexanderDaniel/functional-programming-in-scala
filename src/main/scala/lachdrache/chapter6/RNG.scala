b9809ad04a52f4cf5993b6db5fb27c408948a74c
Alexander Daniel <alexander.daniel@gmx.at>
2014-04-15 21:23:58 +0200

    chapter 6: exercises 3 and 4

--- src/main/scala/lachdrache/chapter6/RNG.scala
+++ src/main/scala/lachdrache/chapter6/RNG.scala
@@ -25,4 +25,40 @@ object RNG {
     (d, rng2)
   }
 
+  /** Exercise 3
+    * [[https://github.com/pchiusano/fpinscala/blob/master/answerkey/state/3.answer.scala answer]]
+    */
+  def intDoublePair(rng0: RNG): ((Int, Double), RNG) = {
+    val (n, rng1) = rng0.nextInt
+    val (d, rng2) = double(rng1)
+    ((n,d), rng2)
+  }
+
+  def doubleIntPair(rng0: RNG): ((Double, Int), RNG) = {
+    val (pair, rng1) = intDoublePair(rng0)
+    (pair.swap, rng1)
+  }
+
+  def double3(rng0: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng0)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1,d2,d3), rng3)
+  }
+
+  /** Exercise 4
+    * [[https://github.com/pchiusano/fpinscala/blob/master/answerkey/state/4.answer.scala answer]]
+    */
+  def int(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @annotation.tailrec
+    def go(n: Int, acc: (List[Int], RNG)): (List[Int], RNG) =
+      (n, acc) match {
+        case (0, _) => acc
+        case (_, (tail, rng0)) =>
+          val (n, rng1) = rng0.nextInt
+          go(n-1, (n :: tail, rng1))
+      }
+    go(count, (Nil, rng))
+  }
+  
 }
