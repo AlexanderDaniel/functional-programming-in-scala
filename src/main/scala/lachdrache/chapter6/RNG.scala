b81456c6517bc341336280bdbc6a296ef9287100
Alexander Daniel <alexander.daniel@gmx.at>
2014-04-20 12:37:35 +0200

    chapter 6: 9

--- src/main/scala/lachdrache/chapter6/RNG.scala
+++ src/main/scala/lachdrache/chapter6/RNG.scala
@@ -127,4 +127,15 @@ object RNG {
       else notNegativeLessThan(n)
     }
 
+  /** Exercise 9
+    * [[https://github.com/pchiusano/fpinscala/blob/master/answerkey/state/11.answer.scala answer]]
+    */
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra) { a =>
+      map(rb)(b => f(a,b))
+    }
+
 }
