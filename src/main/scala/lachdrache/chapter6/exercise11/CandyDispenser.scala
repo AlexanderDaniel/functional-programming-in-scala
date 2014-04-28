134152cb5dd3ef40f65e9775fd42f3ad2679e43d
Alexander Daniel <alexander.daniel@gmx.at>
2014-04-28 17:46:34 +0200

    chapter 6: candy dispenser

--- src/main/scala/lachdrache/chapter6/exercise11/CandyDispenser.scala
+++ src/main/scala/lachdrache/chapter6/exercise11/CandyDispenser.scala
@@ -8,8 +8,8 @@ object CandyDispenser {
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
     inputs.foldLeft(State[Machine, (Int, Int)](s => ((s.coins, s.candies), s))) {
       (z, in) => update(z)(in match {
-        case Coin => m => if (m.candies>0 && m.locked) m.copy(locked=false, coins=m.coins+1) else m
-        case Turn => m => if (!m.locked) m.copy(locked=true, candies = m.candies-1) else m
+        case Coin => m => if (m.candies > 0 && m.locked) m.copy(locked = false, coins = m.coins + 1) else m
+        case Turn => m => if (!m.locked) m.copy(locked = true, candies = m.candies - 1) else m
         case _ => identity
       })
     }
@@ -17,4 +17,34 @@ object CandyDispenser {
   def update(z: State[Machine, (Int, Int)])(f: Machine => Machine): State[Machine, (Int, Int)] =
     z.flatMap(_ => modify(f)).flatMap(_ => get).map(m => (m.coins, m.candies))
 
+  def simulateMachine_Answer(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs.map(i => modify((s: Machine) => (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    })))
+    s <- get
+  } yield (s.coins, s.candies)
+
+  def simulateMachine_AnswerRefactored(inputs: List[Input]): State[Machine, (Int, Int)] =
+    for {
+      _ <- sequence(inputs map inputToState)
+      s <- get
+    } yield (s.coins, s.candies)
+
+
+  def inputToState(i: Input): State[Machine, Unit] =
+    modify((s: Machine) => (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    })
 }
--- src/test/scala/lachdrache/chapter6/exercise11/CandyDispenserSpec.scala
+++ src/test/scala/lachdrache/chapter6/exercise11/CandyDispenserSpec.scala
@@ -1,37 +1,43 @@
 package lachdrache.chapter6.exercise11
 
 import org.specs2.mutable.Specification
-import lachdrache.chapter6.State._
+import CandyDispenser._
+import lachdrache.chapter6.State
 
+/**
+ * [[https://github.com/pchiusano/fpinscala/blob/master/answerkey/state/15.answer.scala answer]]
+ */
 class CandyDispenserSpec extends Specification {
 
-  "empty list of input" should {
-    "not change the state" in {
-      val machine = CandyDispenser.simulateMachine(List())
-      val initialState = Machine(locked = false, coins = 0, candies = 13)
-      machine.run(initialState) === ((0,13), initialState)
+  testCandyDispenser("my own solution using foldLeft", simulateMachine)
+  testCandyDispenser("solution from the authors", simulateMachine_Answer)
+  testCandyDispenser("solution from the authors refactored", simulateMachine_AnswerRefactored)
+
+  def testCandyDispenser(name: String, simulateMachine: List[Input] => State[Machine, (Int, Int)]): Unit = {
+    name should {
+      "empty list of input" should {
+        "not change the state" in {
+          val machine = simulateMachine(List())
+          val initialState = Machine(locked = false, coins = 0, candies = 13)
+          machine.run(initialState) ===((0, 13), initialState)
+        }
+      }
+
+      "Inserting a coin into a locked machine will cause it to unlock if there is any candy left" should {
+        "coin into locked machine with left candy" in {
+          val machine = simulateMachine(List(Coin))
+          val initialState = Machine(locked = true, coins = 0, candies = 13)
+          machine.run(initialState) ===((1, 13), Machine(locked = false, coins = 1, candies = 13))
+        }
+      }
+
+      "Turning the knob on an unlocked machine will cause it to dispense candy and become locked" should {
+        "knob on an unlocked machine" in {
+          val machine = simulateMachine(List(Turn))
+          val initialState = Machine(locked = false, coins = 0, candies = 13)
+          machine.run(initialState) ===((0, 12), Machine(locked = true, coins = 0, candies = 12))
+        }
+      }
     }
   }
-
-  "Inserting a coin into a locked machine will cause it to unlock if there is any candy left" should {
-    "coin into locked machine with left candy" in {
-      val machine = CandyDispenser.simulateMachine(List(Coin))
-      val initialState = Machine(locked = true, coins = 0, candies = 13)
-      machine.run(initialState) === ((1,13), Machine(locked = false, coins=1, candies=13))
-    }
-  }
-
-  "Turning the knob on an unlocked machine will cause it to dispense candy and become locked" should {
-    "knob on an unlocked machine" in {
-      val machine = CandyDispenser.simulateMachine(List(Turn))
-      val initialState = Machine(locked = false, coins = 0, candies = 13)
-      machine.run(initialState) === ((0,12), Machine(locked = true, coins=0, candies=12))
-    }
-  }
-
-//  "coin" should {
-//    {
-//      CandyDispenser.coin(unit((0,0))) === false
-//    }.eg
-//  }
 }
