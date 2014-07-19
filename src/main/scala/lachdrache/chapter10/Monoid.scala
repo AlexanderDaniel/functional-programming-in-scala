7221d072ce58d7b20a89ad095848a4abe963a0c8
Alexander Daniel <alexander.daniel@gmx.at>
2014-07-19 17:19:42 +0200

    chapter 10: exercise 2

--- src/main/scala/lachdrache/chapter10/Monoid.scala
+++ src/main/scala/lachdrache/chapter10/Monoid.scala
@@ -32,4 +32,9 @@ object Monoid {
     val zero: Boolean = true
   }
 
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+    override def zero: Option[A] = None
+  }
+
 }
