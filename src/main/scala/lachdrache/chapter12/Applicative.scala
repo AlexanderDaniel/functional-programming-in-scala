ac6dc20b7e03ed58b856b4526e5e7dfa02456d32
Alexander Daniel <alexander.daniel@gmx.at>
2014-08-28 22:07:11 +0200

    Applicative functors: exercise 3

--- src/main/scala/lachdrache/chapter12/Applicative.scala
+++ src/main/scala/lachdrache/chapter12/Applicative.scala
@@ -45,6 +45,8 @@ trait Applicative[F[_]] extends Functor[F] {
   // exercise 3
   def map3[A,B,C,D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D):F[D] =
     apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+  def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E):F[E] =
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
 
 
 }
