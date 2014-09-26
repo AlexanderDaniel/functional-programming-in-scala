0523bc6ab569393f0ed5fc1955c3d1612ff27616
Alexander Daniel <alexander.daniel@gmx.at>
2014-09-26 09:55:08 +0200

    Chapter 13 external effects and I/O: SimpleIOType

---
+++ src/main/scala/lachdrache/chapter13/SimpleIOType.scala
@@ -0,0 +1,27 @@
+package lachdrache.chapter13
+
+object SimpleIOType {
+
+  trait IO {
+    def run(): Unit
+  }
+
+  def PrintLine(msg: String): IO =
+    new IO {
+      override def run(): Unit = println(msg)
+    }
+
+  def contest(p1: Player, p2: Player): IO =
+    PrintLine(winnerMsg(winner(p1, p2)))
+
+  case class Player(name: String, score: Int)
+
+  def winner(p1: Player, p2: Player): Option[Player] =
+    if (p1.score > p2.score) Some(p1)
+    else if (p1.score < p2.score) Some(p2)
+    else None
+
+  def winnerMsg(p: Option[Player]): String = p map {
+    case Player(name, _) => s"$name is the winner!"
+  } getOrElse "It's a draw."
+}
---
+++ src/test/scala/lachdrache/chapter13/SimpleIOTypeSpec.scala
@@ -0,0 +1,14 @@
+package lachdrache.chapter13
+
+import org.scalatest.FunSpec
+import SimpleIOType._
+
+class SimpleIOTypeSpec extends FunSpec {
+
+  describe("contest") {
+    it("should yield an IO type which can be run to print the result") {
+      val io: IO = contest(Player("MrA", score=3), Player("MrB", score=5))
+      io.run()
+    }
+  }
+}
