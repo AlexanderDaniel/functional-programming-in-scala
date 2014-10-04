8eb4f79071bf2359cc752100d264f74df840c08e
Alexander Daniel <alexander.daniel@gmx.at>
2014-10-04 17:57:30 +0200

    Chapter 13 external effects and I/O: Free

--- src/main/scala/lachdrache/chapter13/IO3.scala
+++ src/main/scala/lachdrache/chapter13/IO3.scala
@@ -1,5 +1,6 @@
 package lachdrache.chapter13
 
+import lachdrache.chapter11.Monad
 import lachdrache.chapter7.Par.Par
 
 import scala.language.higherKinds
@@ -10,17 +11,27 @@ import scala.language.higherKinds
 object IO3 {
 
   sealed trait Free[F[_], A] {
+    // Exercise 13.1 part 1
     def flatMap[B](f: A => Free[F, B]): Free[F, B] =
       FlatMap(this, f)
     def map[B](f: A => B): Free[F, B] =
-      flatMap(f andThen (Return(_)))
+      this flatMap (a => Return(f(a)))
   }
   case class Return[F[_], A](a: A) extends Free[F, A]
   case class Suspend[F[_], A](s: F[A]) extends Free[F, A]
   case class FlatMap[F[_], A, B](s: Free[F, A],
     f: A => Free[F, B]) extends Free[F, B]
 
-  type TailRec[A] = Free[Function0,A]
-  type Async[A] = Free[Par,A]
+  type TailRec[A] = Free[Function0, A]
+  type Async[A] = Free[Par, A]
+
+  // Exercise 13.1 part 2
+  def freeMonad[F[_]]: Monad[({type f[a] = Free[F, a]})#f] =
+    new Monad[({type f[a] = Free[F, a]})#f] {
+      def unit[A](a: => A): Free[F, A] =
+        Return(a)
+      def flatMap[A, B](ma: Free[F, A])(f: (A) => Free[F, B]): Free[F, B] =
+        ma flatMap f
+    }
 
 }
