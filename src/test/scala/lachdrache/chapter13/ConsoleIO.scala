86c434f4fb2e8feec08d23547f789d5ce55fb0b2
Alexander Daniel <alexander.daniel@gmx.at>
2014-10-25 17:17:32 +0200

    Chapter 13 external effects and I/O: running Console IO

---
+++ src/test/scala/lachdrache/chapter13/ConsoleIO.scala
@@ -0,0 +1,15 @@
+package lachdrache.chapter13
+
+import lachdrache.chapter13.IO3._
+
+object ConsoleIO extends App {
+
+  val justConsoleIo: Free[Console, Option[String]] = for {
+    _ <- Console.printLn("I can only interact with the console.")
+    ln <- Console.readLn
+    _ <- Console.printLn(ln.getOrElse(""))
+  } yield ln
+
+  runConsole(justConsoleIo)
+
+}
