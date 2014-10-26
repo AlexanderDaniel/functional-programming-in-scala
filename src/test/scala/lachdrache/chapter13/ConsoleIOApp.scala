7fe28cb76fc98e3532c82b8da6f66ecf1b985f29
Alexander Daniel <alexander.daniel@gmx.at>
2014-10-26 19:50:11 +0100

    Chapter 13 external effects and I/O: improved name of class

--- src/test/scala/lachdrache/chapter13/ConsoleIOApp.scala
+++ src/test/scala/lachdrache/chapter13/ConsoleIOApp.scala
@@ -2,7 +2,7 @@ package lachdrache.chapter13
 
 import lachdrache.chapter13.IO3._
 
-object ConsoleIO extends App {
+object ConsoleIOApp extends App {
 
   val justConsoleIo: Free[Console, Option[String]] = for {
     _ <- Console.printLn("I can only interact with the console.")
