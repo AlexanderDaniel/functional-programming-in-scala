750d39ef4058e3f6fc22ab62b69bb91e6ab8a792
Alexander Daniel <alexander.daniel@gmx.at>
2014-11-01 17:14:39 +0100

    Chapter 14 local effects and mutable state: exercise 14.1

---
+++ src/main/scala/lachdrache/chapter14/LocalEffects.scala
@@ -0,0 +1,89 @@
+package lachdrache.chapter14
+
+sealed trait ST[S,A] { self =>
+  protected def run(s: S): (A,S)
+  def map[B](f: A => B): ST[S,B] = new ST[S,B] {
+    def run(s: S) = {
+      val (a, s1) = self.run(s)
+      (f(a), s1)
+    }
+  }
+  def flatMap[B](f: A => ST[S,B]): ST[S,B] = new ST[S,B] {
+    def run(s: S) = {
+      val (a, s1) = self.run(s)
+      f(a).run(s1)
+    }
+  }
+}
+
+object ST {
+  def apply[S,A](a: => A) = {
+    lazy val memo = a
+    new ST[S,A] {
+      def run(s: S) = (memo, s)
+    }
+  }
+  def runST[A](st: RunnableST[A]): A =
+    st[Unit].run(())._1
+}
+
+sealed trait STRef[S,A] {
+  protected var cell: A
+  def read: ST[S,A] = ST(cell)
+  def write(a: => A): ST[S,Unit] = new ST[S,Unit] {
+    def run(s: S) = {
+      cell = a
+      ((), s)
+    }
+  }
+}
+
+object STRef {
+  def apply[S,A](a: A): ST[S, STRef[S,A]] = ST(new STRef[S,A] {
+    var cell = a
+  })
+}
+
+trait RunnableST[A] {
+  def apply[S]: ST[S,A]
+}
+
+sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
+  protected def value: Array[A]
+  def size: ST[S,Int] = ST(value.size)
+
+  def write(i: Int, a: A): ST[S,Unit] = new ST[S, Unit] {
+    def run(s: S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  def read(i: Int): ST[S,A] = ST(value(i))
+
+  def freeze: ST[S,List[A]] = ST(value.toList)
+
+  def noop: ST[S, Unit] = new ST[S, Unit] {
+    override protected def run(s: S): (Unit, S) = {
+      ((), s)
+    }
+  }
+
+  // Exercise 14.1
+  def fill(xs: Map[Int,A]): ST[S,Unit] = {
+    xs.foldLeft(ST[S,Unit](())) { case (z, (k, v)) =>
+      z flatMap (_ => write(k, v))
+    }
+  }
+
+}
+
+object STArray {
+  def apply[S,A:Manifest](sz: Int, v: A): ST[S, STArray[S,A]] =
+    ST(new STArray[S,A] {
+      lazy val value = Array.fill(sz)(v)
+    })
+}
+
+
+
